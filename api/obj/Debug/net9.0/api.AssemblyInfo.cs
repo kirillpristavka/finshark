415c55c2775f1af4d0e122d23d1d823b0b5d0edf
Kirill <kirill.pristavka31@yandex.ru>
2025-04-13 10:19:14 +0200

    contextAuthJWT

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620e1270ddf0df65ea1b9acdeb1dcc8727627213")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f4cdc7b80e3a0711e8a039d35ce65d24a6265fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -8,25 +8,41 @@ import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
 import DesignPage from "../Pages/DesignPage/DesignPage";
 import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
 import CashFlowStatement from "../Components/CashFlowStatement/CashFlowStatement";
+import LoginPage from "../Pages/LoginPage/LoginPage";
+import RegisterPage from "../Pages/RegisterPage/RegisterPage";
+import ProtectedRoute from "./ProtectedRoute";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "register", element: <RegisterPage /> },
+      {
+        path: "search",
+        element: (
+          <ProtectedRoute>
+            <SearchPage />
+          </ProtectedRoute>
+        ),
+      },
+      { path: "design-guide", element: <DesignPage /> },
+      {
+        path: "company/:ticker",
+        element: (
+          <ProtectedRoute>
+            <CompanyPage />
+          </ProtectedRoute>
+        ),
         children: [
-            { path: "", element: <HomePage /> },
-            { path: "search", element: <SearchPage /> },
-            { path: "design-guide", element: <DesignPage /> },
-            { 
-              path: "company/:ticker",
-              element: <CompanyPage/>,
-              children: [
-                  { path: "company-profile", element: <CompanyProfile /> },
-                  { path: "income-statement", element: <IncomeStatement /> },
-                  { path: "balance-sheet", element: <BalanceSheet /> },
-                  { path: "cashflow-statement", element: <CashFlowStatement /> },
-              ],
-            },
-        ]
-    }
+          { path: "company-profile", element: <CompanyProfile /> },
+          { path: "income-statement", element: <IncomeStatement /> },
+          { path: "balance-sheet", element: <BalanceSheet /> },
+          { path: "cashflow-statement", element: <CashFlowStatement /> },
+        ],
+      },
+    ],
+  },
 ]);
